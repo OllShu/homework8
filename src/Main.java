a3ef08700a674569cc0e90a37f686c89141be423
dns <shulgaol@mail.ru>
2022-12-05 13:17:46 +0200

    задача 1 домашки по массивам, часть 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] integers;
+        integers = new int[3];
+        integers [0] = 1;
+        integers [1] = 2;
+        integers [2] = 3;
+
+        double [] fractionalNumbers = {1.57, 7.654, 9.986};
+
+        int [] arbitraryArray = {2, 4, 5, 7, 88, 105};
+    }
+}
